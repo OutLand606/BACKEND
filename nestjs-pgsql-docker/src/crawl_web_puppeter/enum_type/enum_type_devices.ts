7e2ba135295e94a9a12cc8d0510049c4036a88a6
OutLand <hatanduy123@gmail.com>
2024-09-16 09:50:41 +0700

    configControllerCrawlWeb

---
+++ nestjs-pgsql-docker/src/crawl_web_puppeter/enum_type/enum_type_devices.ts
@@ -0,0 +1,12 @@
+export enum Devices {
+  Desktop = 'desktop',
+  Mobile = 'mobile',
+}
+
+export enum OperatingSystems {
+  Windows = 'windows',
+  MacOS = 'macos',
+  Linux = 'linux',
+  Android = 'android',
+  iOS = 'ios',
+}
