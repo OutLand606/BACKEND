85cc898ad144952a83b43d9d982d214c20d36358
OutLand <hatanduy123@gmail.com>
2024-09-20 13:57:27 +0700

    addControllEmulator

--- nestjs-pgsql-docker/src/android/android.service.ts
+++ nestjs-pgsql-docker/src/android/android.service.ts
@@ -1,11 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
 import { promisify } from 'util';
 
 const execPromise = promisify(exec);
 
 @Injectable()
 export class AndroidService {
+  async startAllAvds(): Promise<string[]> {
+    try {
+      const { stdout: listOutput } = await execPromise('emulator -list-avds');
+      const avdList = listOutput.split('\n').filter((avd) => avd.trim() !== '');
+      if (avdList.length === 0) {
+        throw new Error('Không tìm thấy máy ảo nào.');
+      }
+      const startPromises = avdList.map(async (avdName) => {
+        return new Promise<string>((resolve, reject) => {
+          const process = spawn('emulator', ['-avd', avdName], {
+            detached: true,
+            stdio: 'ignore',
+          });
+          process.on('error', (error) => {
+            reject(`Lỗi khi khởi động AVD ${avdName}: ${error.message}`);
+          });
+          process.unref();
+          resolve(`Máy ảo ${avdName} đang được khởi động độc lập.`);
+        });
+      });
+
+      const results = await Promise.all(startPromises);
+      return results;
+    } catch (error) {
+      throw new Error(
+        `Lỗi khi lấy danh sách AVD hoặc khởi động AVD: ${error.message}`,
+      );
+    }
+  }
+
   async getActiveDevices(): Promise<string[]> {
     try {
       const { stdout, stderr } = await execPromise('adb devices');
@@ -55,33 +87,76 @@ export class AndroidService {
     }
   }
 
+  async installApkOnAllDevices(
+    buffer: Buffer,
+    originalname: string,
+  ): Promise<string[]> {
+    const uploadDir = path.join(__dirname, '../../uploads');
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+    const tempFilePath = path.join(uploadDir, originalname);
+    fs.writeFileSync(tempFilePath, buffer);
+    try {
+      const devices = await this.getActiveDevices();
+      const results = [];
+      for (const device of devices) {
+        try {
+          const { stderr } = await execPromise(
+            `adb -s ${device} install -r ${tempFilePath}`,
+          );
+          if (stderr) {
+            results.push(`Lỗi khi cài đặt ${originalname} trên ${device}: ${stderr}`);
+          } else {
+            results.push(
+              `Cài đặt APK ${originalname} thành công trên ${device}`,
+            );
+          }
+        } catch (installError) {
+          results.push(
+            `Lỗi cài đặt trên thiết bị ${device}: ${installError.message}`,
+          );
+          continue;
+        }
+      }
+      return results;
+    } catch (error) {
+      throw new Error(`Lỗi khi cài đặt APK ${originalname}: ${error.message}`);
+    } finally {
+      fs.unlinkSync(tempFilePath);
+    }
+  }
+
   async executeCommands(
     deviceIds: string[],
     commands: string[],
   ): Promise<string> {
-    for (const deviceId of deviceIds) {
-      for (const command of commands) {
-        const fullCommand = `adb -s ${deviceId} shell ${command}`;
-        try {
-          const { stdout, stderr } = await execPromise(fullCommand);
-          if (stderr) {
-            throw new Error(stderr);
-          }
-          console.log(`Output from device ${deviceId}: ${stdout}`);
-        } catch (error) {}
+    if (deviceIds.length > 0) {
+      for (const deviceId of deviceIds) {
+        for (const command of commands) {
+          const fullCommand = `adb -s ${deviceId} shell ${command}`;
+          try {
+            const { stdout, stderr } = await execPromise(fullCommand);
+            if (stderr) {
+              throw new Error(stderr);
+            }
+            console.log(`Output from device ${deviceId}: ${stdout}`);
+          } catch (error) {}
+        }
       }
+      return 'Commands executed successfully on all devices';
+    } else {
+      return 'There are no devices running on the computer';
     }
-    return 'Commands executed successfully on all devices';
   }
 
-  async navigateToUrl(url: string): Promise<string> {
+  async navigateToUrl(body): Promise<string> {
     const listDevices = await this.getActiveDevices();
-    const listPackagesInstallDevice = await this.getInstalledApps(listDevices);
     const commands = [
-      //   `am start -a android.intent.action.VIEW -d ${url}`,
-      'monkey -p com.google.android.youtube -c android.intent.category.LAUNCHER 1',
-      'shell input tap 600 200',
-    //   'am force-stop com.google.android.youtube',
+      //   `am start -a android.intent.action.VIEW -d ${body.url}`,
+      // 'monkey -p com.google.android.youtube -c android.intent.category.LAUNCHER 1',
+      // 'am force-stop com.google.android.youtube',
+      'monkey -p org.telegram.messenger.web -c android.intent.category.LAUNCHER 1',
     ];
 
     return await this.executeCommands(listDevices, commands);
