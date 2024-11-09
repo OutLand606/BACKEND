ed37825e93ac8c257e95561c88931ca109c8db3c
OutLand <hatanduy123@gmail.com>
2024-11-09 10:44:14 +0700

    updateImportFileTxT

--- nestjs-pgsql-docker/src/crawl_web_puppeter/crawl_web_puppeter.controller.ts
+++ nestjs-pgsql-docker/src/crawl_web_puppeter/crawl_web_puppeter.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Post, Body, UseGuards, Query } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Query,
+  BadRequestException,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { CrawlWebPuppeterService } from './crawl_web_puppeter.service';
-import { crawlWebDTO } from './dto/crawl_web.dto';
 import { Public } from 'src/auth/metadata';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Readable } from 'stream';
+import * as readline from 'readline';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Controller('crawl-web-puppeter')
 export class CrawlWebPuppeterController {
@@ -48,6 +61,32 @@ export class CrawlWebPuppeterController {
   @Post('run-scripts-profile-chrome')
   async runScript(@Query() query: { profileNames: string; scriptsName: any }) {
     const { profileNames, scriptsName } = query;
-    return this.service.runScriptOnProfile(profileNames, scriptsName);
+    const profileNamesRunScript = profileNames ? profileNames.split(',') : [];
+    await this.service.runScriptOnProfile(profileNamesRunScript, scriptsName);
+    return { message: 'Đã chạy script trên các profile thành công!' };
+  }
+
+  @Public()
+  @Post('import-file-txt')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      limits: { fileSize: 10 * 1024 * 1024 }, // Giới hạn 10 MB mỗi file
+      fileFilter: (req, file, callback) => {
+        if (!file.originalname.match(/\.txt$/)) {
+          return callback(
+            new BadRequestException('Chỉ cho phép tệp TXT!'),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  async importFileTxT(@UploadedFile() file) {
+    const jsonPath =  await this.service.importFileTxt(file);
+    return {
+      message: 'File TXT đã được chuyển đổi và lưu thành JSON thành công!',
+      filePath: jsonPath,
+    };
   }
 }
--- nestjs-pgsql-docker/src/crawl_web_puppeter/dto/crawl_web.dto.ts
+++
@@ -1,4 +0,0 @@
-export class crawlWebDTO {
-  url: string;
-  parse_description: string;
-}
